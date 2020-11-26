678c0aaea354cddb90cfc9756ab32c7815cae3f2
fozeya1 <fozeya620@gmail.com>
2020-11-25 20:39:27 -0500

    Update stockWatcher.py
    working on Step A: Setup Threading and Logging

--- stockWatcher.py
+++ stockWatcher.py
@@ -31,3 +31,36 @@ def main():
         thread.setDaemon(True)
         thread.start()
     time.sleep(5) # Sleep for threads to print msgs
+    # Need a try-except to catch the ctrl-c ...
+    try:
+        input("\nHit CTRL-C to stop recording.\n\n")
+    except:
+        pass
+    logging.info("StockWatcher - end program")
+
+def get_quote(symbol):
+ """
+ Get a stock quote for the given stock symbol using
+ Python requests and BeautifulSoup modules.
+ Determine the availability of webpage
+ Request the quote every WAIT_INTERVAL minutes until the
+ user ends the program with CTRL-C
+ Compare current quote with previous quote and send
+ text when different.
+ """
+
+def get_prices(stocks):
+    format_url = "http://finance.yahoo.com/d/quotes.csv?s=%s&f=l1"
+    stock_string = ",".join(stocks)
+    url = format_url % stock_string
+    prices = [float(x) for x in requests.get(url).content.split()]
+    price_dict = dict(zip(stocks, prices))
+    return price_dict
+
+prices = ['20', '25', '30', '30', '30', '20']
+price = prices[0]
+prev_price = '10'
+
+text = "Start watching " + symbol + ": Price: " + price
+print(text)
+logging.info(text)
