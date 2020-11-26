a4a1a732a399d368a4fd9161b00071f801837c39
fozeya1 <fozeya620@gmail.com>
2020-11-25 20:27:05 -0500

    Update stockWatcher.py
    working on Step A: Setup Threading and Logging

--- stockWatcher.py
+++ stockWatcher.py
@@ -1,4 +1,33 @@
-def concat_even(n):
-    even_number = [str(i) for i in n if i % 2 == 0]
-    return ''.join(even_number)
-print(concat_even([1,2,3,4,5,6,7,8])) 
+import sys, time
+import requests, bs4
+import threading, logging
+from random import *
+
+
+# constants used
+WAIT_INTERVAL = 120
+LOG_FILE = 'stockPriceLog.txt'
+
+def main():
+    """
+    Clear the log, set up logging, log a start message
+    Retrieve the list of stock symbols from the command
+    line
+    Start up a thread for each stock listed
+    Sleep a little to allow the stock prices to be logged
+    Have the user enter CTRL-C to stop the program.
+    """
+
+    open(LOG_FILE, 'w').close()
+    logging.basicConfig(filename=LOG_FILE,level=logging.INFO,format=' %(asctime)s %(message)s ')
+    logging.info("StockWatcher - start program")
+
+    stock_list = ["MSFT","GOOG","AAPL","AMZN"]
+
+    for i in range(len(stock_list)):
+        stock = stock_list[i].upper()
+        print("Begin watch for " + stock)
+        thread = threading.Thread(target = get_quote,args = (stock, ))
+        thread.setDaemon(True)
+        thread.start()
+    time.sleep(5) # Sleep for threads to print msgs
